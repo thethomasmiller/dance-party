2891cdfbe62ba8b83f7c7d645e69ce5459d6fd5e
Thomas Miller <thomasmiller@Thomass-MacBook-Air.local>
2020-07-06 10:30:42 -0400

    Fix Video Link

--- src/Boombox.js
+++ src/Boombox.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Boombox() {
   return (
     <div className="boombox">
-       <iframe  width="280" height="157" src="https://www.youtube.com/embed/Tsna_zk8X1E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+       <iframe width="280" height="157" src="https://www.youtube.com/embed/IkX3qWC95ko" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   )
 }
